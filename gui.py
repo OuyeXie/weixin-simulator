228022f2a6cfba911d5fae5ad210bd4bd3dc918a
Ouye Xie <o@shengupiao.com>
2015-07-08 16:12:48 +0800

    for neeqsub

--- gui.py
+++ gui.py
@@ -17,7 +17,7 @@ settings = {
     "FromUserName": "oLXjgjiWeAS1gfe4ECchYewwoyTc",
 
     # URL of your Wexin handler.
-    "url": "http://localhost:3001/callback/neeqsub”,
+    "url": "http://localhost:3001/callback/neeqsub",
 
     # These will be displayed in GUI.
     "mp_display_name": "APP",
